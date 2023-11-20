9482c4c28687ed0b4eaef6edd80a6fb1f38ce21e
Shikshya Sharma <144874211+SharmaSimm@users.noreply.github.com>
2023-11-20 12:42:16 -0800

    Bill.py

--- Bill.py
+++ Bill.py
@@ -1,3 +1,10 @@
+"""
+Bill.py defines the Bill class as a subclass of 'Expenses',specifically for handling bill payments.
+tThe class constructors sets the expense category to "Bill" & accepts a company name and amount. 
+It provides a customized string representation that includes the company name and bill amount. 
+This class illustrates the application of inheritance in Python, extending Expenses class to cater to a specific type of financail activity. 
+
+"""
 import Expenses
 
 class Bill(Expenses):
