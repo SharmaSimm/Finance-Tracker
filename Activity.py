41b3e1e3336dcfb8c04d2f2a4e800a6f17a8d7ab
Shikshya Sharma <144874211+SharmaSimm@users.noreply.github.com>
2023-11-20 12:45:19 -0800

    Activity.py

--- Activity.py
+++ Activity.py
@@ -1,4 +1,10 @@
+"""
+Activity.py defines the 'Activity' subclass of 'Expenses', focusing on activity -related expenses. 
+It includes location details and overrides the string representation to display the activity type,
+location and amount. The class showcases Python inheritance by extending the 'Expenses' class for specific activity expenses.
 
+
+"""
 import Expenses
 
 class Activity(Expense)
