963e7f83e14e646538d25dd5d98dcbfd97619eec
Shikshya Sharma <144874211+SharmaSimm@users.noreply.github.com>
2023-11-20 12:20:14 -0800

    Expenses.py

--- Expenses.py
+++ Expenses.py
@@ -1,5 +1,11 @@
+"""
+This class manages individual expense records. The class encapsulates expense details such as category and amount, 
+offering both getters and setters for these properties. Its key functionality lies in enabling the creation 
+and manipulation of expense data, effectively represented through a clear string output format. 
+This class forms an integral part of a larger financial management system, using basic principles of OOP IN Python. 
 
 
+"""
 class Expenses():
 
 
