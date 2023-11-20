bdeaad75fe65e58b64d086eb5ab66250a9e0af18
Shikshya Sharma <144874211+SharmaSimm@users.noreply.github.com>
2023-11-20 12:46:50 -0800

    Food.py

--- Food.py
+++ Food.py
@@ -1,4 +1,11 @@
+"""
+Food.py creates the Food class, a subclass of Expenses, dedicated to tracking food-related expenses.
+It adds a specific food item name to the expense details and customizes the string representation
+to include both the food category and the item name, along with the expense amount.
+This class exemplifies Python inheritance, extending the Expenses class functionality to cater specifically to food expenses.
 
+
+"""
 import Expenses
 
 class Food(Expenses):
